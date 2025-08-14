3dc165fb9e8b4c88deef5f40e5a0d5194e736301
sofiasarak <sofiasarak30@gmail.com>
2025-08-14 14:26:50 -0700

    logistic growth again w pretty plot

--- function_practice.R
+++ function_practice.R
@@ -83,3 +83,67 @@ df_means(df=mtcars)
 df_means(palmerpenguins::penguins)
 
 
+#logistic growth example
+
+#logistic growth equation
+logistic_growth <- function(N0, K, r, time) {
+  Nt <- K / (1 + ((K-N0)/N0) * exp(-r * time))
+  print(Nt)
+}
+
+# check for one set of values (whether or not that basic function worked)
+logistic_growth(N0 =100, K=6000, r=0.27, time=40)
+
+
+# working on example just dealing with time
+time_vec <- seq(0, 35, by=0.1)
+
+#apply the logistic growth function to that vector
+pop_35 <-  logistic_growth(N0=100, K=6000, r=0.27, time=time_vec)
+
+#turn into a data frame; combining time setps and population size
+pop_time_35 <- data.frame(time_vec, pop_35)
+
+#plot it!
+library(ggplot2)
+ggplot(data=pop_time_35, aes(x=time_vec, y=pop_35)) + geom_line(linewidth=0.5) #looks like logistic growth function!
+
+#alternatively, with an internal for loop
+pop_35_vector <- vector(mode="numeric", length=length(time_vec))
+
+for(i in seq_along(time_vec)) {
+population <-  logistic_growth(N0=100, K=6000, r=0.27, time=time_vec[i])
+pop_35_vector[i] <- population
+}
+
+# now building to estimating across growth rates
+#creating series of growth rates 
+r_seq <- seq(from = 0.2, to=0.4, by=0.01)
+
+#creating a MATRIX to store output values
+out_matrix <- matrix(nrow = length(time_vec), ncol = length(r_seq))
+
+for(i in seq_along(time_vec)) {
+  for (j in seq_along(r_seq)){ #doesn't seem to make a difference output-wise which order you index in 
+  population <-  logistic_growth(N0=100, K=6000, 
+                                 r=r_seq[j], time=time_vec[i])
+  out_matrix[i,j] <- population #need two dimensions for where we're going to store because it's a matrix
+  }
+}
+
+#data wrangling to get a plot
+#adding time as a variable
+out_df <- data.frame(out_matrix, time=time_vec) #make it a data frame and add time
+
+#updating column names for growth rates
+colnames(out_df) <- c(paste0("gr_", r_seq),"time") #using paste to make it something like "gr_0.21"!
+
+#pivot longer to make it tidy; we like longer data instead of wider data!
+library(tidyverse)
+out_df_long <- out_df |> 
+  pivot_longer(cols = -time, 
+               names_to = "growth_rate", 
+               values_to = "population")
+
+#time to plot!
+ggplot(data=out_df_long, aes(x=time, y=population)) + geom_line(aes(color = growth_rate)) + theme_minimal()
